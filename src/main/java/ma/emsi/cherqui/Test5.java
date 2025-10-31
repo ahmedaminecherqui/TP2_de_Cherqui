60c113278b1e734ab9d22ff01fd603f0bd2e22ca
ahmedaminecherqui <cherqui2003@gmail.com>
2025-10-31 22:59:47 +0100

    Test 5 effectué avec succes : j'ai ajouter un pdf parser au document loader pour pouvoir charger le pdf en texte au lieu de le charger et lire en binaire

--- src/main/java/ma/emsi/cherqui/Test5.java
+++ src/main/java/ma/emsi/cherqui/Test5.java
@@ -2,26 +2,25 @@ package ma.emsi.cherqui;
 
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.request.ResponseFormat;
-
-import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
-import dev.langchain4j.model.googleai.GoogleAiEmbeddingModel;
-
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiEmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
+
 import java.time.Duration;
+import java.util.Scanner;
 
-public class Test4 {
-
+public class Test5 {
     interface Assistant {
         String chat(String userMessage);
     }
@@ -31,7 +30,7 @@ public class Test4 {
         //  Load Gemini API key
         String apiKey = System.getenv("GEMINI_API_KEY");
 
-//  Chat model (LLM)
+        //  Chat model (LLM)
         ChatModel chatModel = GoogleAiGeminiChatModel.builder()
                 .apiKey(apiKey)
                 .modelName("gemini-2.5-flash")  // works with LangChain4j
@@ -47,7 +46,8 @@ public class Test4 {
                 .build();
 
         // Load a document from local file system
-        Document document = FileSystemDocumentLoader.loadDocument("infos.txt");
+        Document document = FileSystemDocumentLoader.loadDocument("langchain4j.pdf",
+                new ApachePdfBoxDocumentParser());
 
         // Create an in-memory vector store
         EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
@@ -69,17 +69,29 @@ public class Test4 {
                         .build();
 
         // Build the assistant with RAG + memory
-        Assistant assistant = AiServices.builder(Assistant.class)
+                Assistant assistant = AiServices.builder(Assistant.class)
                 .chatModel(chatModel)
                 .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
                 .contentRetriever(retriever)
                 .build();
 
         // Ask a question that must be answered from infos.txt
-        String question = "Pierre appelle son chat. Qu'est-ce qu'il pourrait dire ?";
-        String answer = assistant.chat(question);
-
-        System.out.println("Réponse : " + answer);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
     }
 }
-
